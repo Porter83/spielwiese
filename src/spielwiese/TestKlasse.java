cce099137cbf23c767435c156c103b4372167494
Porter83 <sebastian.sawatzki@gmail.com>
2015-07-02 22:10:55 +0200

    Changed Testklasse

--- src/spielwiese/TestKlasse.java
+++ src/spielwiese/TestKlasse.java
@@ -1,5 +1,13 @@
 package spielwiese;
 
 public class TestKlasse {
+	public static void main(String args[])
+	{
+		int amount = Integer.valueOf(args[0]); 
+		for (int i=0;i < amount;i++)
+		{
+			System.out.println("Teschd");
+		}
+	}
 
 }
